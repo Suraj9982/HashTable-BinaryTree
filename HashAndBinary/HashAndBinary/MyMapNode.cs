c85e020016b3b575cb4be925f5397fbfbd1d29a4
suraj9982 <surajsk9982@gmail.com>
2021-11-23 21:59:54 +0530

    [suraj] add : This find the frequency of words

---
+++ HashAndBinary/HashAndBinary/MyMapNode.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashAndBinary
+{
+    class MyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] items;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int Getarrayposition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = Getarrayposition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.key.Equals(key))
+                {
+                    return item.value;
+                }
+            }
+            return default(V);
+        }
+        public void Add(K key, V value)
+        {
+            int position = Getarrayposition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { key = key, value = value };
+            linkedlist.AddLast(item);
+        }
+        public void Remove(K key)
+        {
+            int position = Getarrayposition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            bool itemfound = false;
+            KeyValue<K, V> founditem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                if (item.key.Equals(key))
+                {
+                    itemfound = true;
+                    founditem = item;
+                }
+            }
+            if (itemfound)
+            {
+                linkedlist.Remove(founditem);
+            }
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int postion)
+        {
+            LinkedList<KeyValue<K, V>> linkedlst = items[postion];
+            if (linkedlst == null)
+            {
+                linkedlst = new LinkedList<KeyValue<K, V>>();
+                items[postion] = linkedlst;
+            }
+            return linkedlst;
+        }
+    }
+    public struct KeyValue<k, v>
+    {
+        public k key { get; set; }
+        public v value { get; set; }
+    }
+}
---
+++ HashAndBinary/HashAndBinary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HashAndBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("1", "to");
+            hash.Add("2", "be");
+            hash.Add("3", "or");
+            hash.Add("4", "not");
+            hash.Add("5", "to");
+            hash.Add("6", "be");
+            string hash3 = hash.Get("3");
+            Console.WriteLine("value of 5th index is: {0}",hash3);
+            hash.Remove("4"); // It will remove the 4th index value.
+        }
+    }
+}
