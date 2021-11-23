d4a52a642b55ab92b951a92a45a84350e31ff45a
suraj9982 <surajsk9982@gmail.com>
2021-11-23 23:32:04 +0530

    [suraj] add : it creates the binary tree

---
+++ HashAndBinary/HashAndBinary/BinarySearchTree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashAndBinary
+{
+    class BinarySearchTree
+    {
+        static readonly int COUNT = 10;
+        public Node root;
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public void Insert(int value)
+        {
+            Node newnode = new Node();
+            newnode.data = value;
+            if (root == null)
+            {
+                root = newnode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (value < current.data)
+                    {
+                        current = current.left;
+                        if (current == null)
+                        {
+                            parent.left = newnode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if (current == null)
+                        {
+                            parent.right = newnode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        public void Pre_order(Node parent)
+        {
+            if (parent != null)
+            {
+                Console.WriteLine(parent.data + " ");
+                Pre_order(parent.left);
+                Pre_order(parent.right);
+            }
+        }
+        public void Post_order(Node parent)
+        {
+            if (parent != null)
+            {
+                Post_order(parent.left);
+                Post_order(parent.right);
+                Console.WriteLine(parent.data + " ");
+            }
+        }
+        public void In_order(Node parent)
+        {
+            if (parent != null)
+            {
+                In_order(parent.left);
+                Console.WriteLine(parent.data + " ");
+                In_order(parent.right);
+            }
+        }
+    }
+}
--- HashAndBinary/HashAndBinary/Program.cs
+++ HashAndBinary/HashAndBinary/Program.cs
@@ -6,17 +6,44 @@ namespace HashAndBinary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hash Table");
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            hash.Add("1", "to");
-            hash.Add("2", "be");
-            hash.Add("3", "or");
-            hash.Add("4", "not");
-            hash.Add("5", "to");
-            hash.Add("6", "be");
-            string hash3 = hash.Get("3");
-            Console.WriteLine("value of 5th index is: {0}",hash3);
-            hash.Remove("4"); // It will remove the 4th index value.
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose option\n1.find frequency of word\n2.Create Binary Tree\n3.exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+                        hash.Add("1", "to");
+                        hash.Add("2", "be");
+                        hash.Add("3", "or");
+                        hash.Add("4", "not");
+                        hash.Add("5", "to");
+                        hash.Add("6", "be");
+                        string hash3 = hash.Get("3");
+                        Console.WriteLine("value of 5th index is: {0}", hash3);
+                        hash.Remove("4"); // It will remove the 4th index value.
+                        break;
+                    case 2:
+                        BinarySearchTree binary = new BinarySearchTree();
+                        binary.Insert(56);
+                        binary.Insert(30);
+                        binary.Insert(70);
+                        binary.Insert(40);
+                        binary.Insert(20);
+                        binary.Pre_order(binary.root);
+                        binary.Post_order(binary.root);
+                        binary.In_order(binary.root);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("your option should be between 1 to 3");
+                        break;
+                }
+            }
         }
     }
 }
